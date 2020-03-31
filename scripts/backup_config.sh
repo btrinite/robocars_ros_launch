25697e2a592cfeff169d7a71471a50c08a995839
Benoit TRINITE <btrinite@free.fr>
2020-03-31 19:15:13 +0200

    script and more nodes

---
+++ scripts/backup_config.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rosparam dump ~/config.yaml
---
+++ scripts/qualibrate_mode.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+rostopic pub /robocars_actuator_ctrl_mode robocars_msgs/robocars_actuator_ctrl_mode -1 -f enter_qualibrate_mode.yaml
+echo "Now in qualibration mode"
+echo "Move remote control al directions now for 10 seconds !"
+sleep 10
+rostopic pub /robocars_actuator_ctrl_mode robocars_msgs/robocars_actuator_ctrl_mode -1 -f leave_qualibrate_mode.yaml
+echo "End of qualibration mode"
+echo "Qualibration result for Seeting :"
+rosparam get /steering_ctrl/command_input_min
+rosparam get /steering_ctrl/command_input_max
+echo "Qualibration result for Throttling :"
+rosparam get /throttling_ctrl/command_input_min
+rosparam get /throttling_ctrl/command_input_max
+
+
+
