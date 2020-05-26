4eb9fd603437eebc140af4a986ba5b57695485c2
Benoit TRINITE <btrinite@free.fr>
2020-05-26 19:00:28 +0200

    settings for rosserial speed

--- launch/startAll.sh
+++ launch/startAll.sh
@@ -7,13 +7,17 @@ if [[ ! -v ROBOCARS_COMPANION_TTY ]]; then
 	echo "env var ROBOCARS_COMPANION_TTY is missing !"
 	exit -1
 fi
+if [[ ! -v ROBOCARS_COMPANION_TTY_SPEED ]]; then
+	echo "env var ROBOCARS_COMPANION_TTY_SPEED is missing !"
+	exit -1
+fi
 if [[ ! -v ROBOCARS_FRONT_VIDEO ]]; then
 	echo "env var ROBOCARS_FRONT_VIDEO is missing !"
 	exit -1
 fi
 
 echo "Main PATH : $ROBOCARS_PATH"
-echo "Companion on tty :  $ROBOCARS_COMPANION_TTY"
+echo "Companion on tty :  $ROBOCARS_COMPANION_TTY ($ROBOCARS_COMPANION_TTY_SPEED)"
 echo "Front video from dev : $ROBOCARS_FRONT_VIDEO"
 
 
